07af8d69b312839d63c2a4ee455e35cc0377554c
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-31 01:04:35 +0200

    test deploy on github

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ copyright = '2022, Hong-Phuc Bui'
 author = 'Hong-Phuc Bui'
 
 # The short X.Y version
-version = ''
+version = '3'
 # The full version, including alpha/beta/rc tags
 release = '3.0.3'
 
@@ -59,7 +59,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -67,7 +67,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = "default"
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -81,7 +81,10 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "font_family": 'Frutiger, "Frutiger Linotype", Univers, Calibri, "Gill Sans", "Gill Sans MT", "Myriad Pro", Myriad, "DejaVu Sans Condensed", "Liberation Sans", "Nimbus Sans L", Tahoma, Geneva, "Helvetica Neue", Helvetica, Arial, sans-serif;',
+    "code_font_size": "smaller"
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
